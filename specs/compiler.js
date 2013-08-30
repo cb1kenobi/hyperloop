10a7f71d228251b7c06603f231a26a67af573090
Jeff Haynie <jhaynie@appcelerator.com>
2013-08-30 15:57:22 -0700

    more compiler test cases

--- specs/compiler.js
+++ specs/compiler.js
@@ -5,8 +5,8 @@ var should = require('should'),
     path = require('path'),
     compiler = require(path.join(__dirname,'..','lib','compiler.js')),
     sf = path.join(__dirname,'..','lib','sourcefile.js'),
+    Uglify = require('uglify-js'),
     SourceFile = require(sf).SourceFile;
-
 
 function MockSourceFile() {
 	SourceFile.call(this, 'test', 'test');
@@ -28,6 +28,15 @@ MockSourceFile.prototype.parseImport = function(node, value) {
 	}
 };
 
+function toJS(obj){ 
+	var source = '('+JSON.stringify(obj)+')',
+    	ast = Uglify.parse(source,{
+        	filename:'<source>'
+    	}),
+    	code = ast.print_to_string({ beautify: 0 });
+    return code.substring(0,code.length-1); // remove semicolon
+}
+
 describe("compiler", function() {
 
 
@@ -41,7 +50,7 @@ describe("compiler", function() {
 		var source = "@import('UIKit/UIButton')",
 			sourcefile = new MockSourceFile(),
 			ast = compiler.compile(source, 'test', sourcefile),
-			code = ast.print_to_string({beautify:true});
+			code = compiler.compress(ast,{},'test');
 		code.should.be.empty;
 		sourcefile.name.should.be.eql('test');
 		sourcefile.filename.should.be.eql('/test');
@@ -57,6 +66,47 @@ describe("compiler", function() {
 		sourcefile.symbols[0].should.have.property('source');
 		sourcefile.symbols[1].should.have.property('source');
 		sourcefile.symbols[1].source.should.be.eql('hyperloop_import("UIKit/UIButton")');
+		sourcefile.symbols[0].type.should.be.eql('package');
+		sourcefile.symbols[1].type.should.be.eql('symbol');
+		sourcefile.symbols[0].value.should.be.eql('UIKit');
+		sourcefile.symbols[1].value.should.be.eql('UIButton');
+		sourcefile.symbols[1].should.have.property('framework');
+		sourcefile.symbols[1].framework.should.be.eql('UIKit');
+	});
+
+	it("should transform simple @native", function() {
+		var source = "@compiler({})",
+			sourcefile = new MockSourceFile(),
+			ast = compiler.compile(source, 'test', sourcefile),
+			code = compiler.compress(ast,{},'test');
+		code.should.be.empty;
+		sourcefile.name.should.be.eql('test');
+		sourcefile.filename.should.be.eql('/test');
+		sourcefile.dirname.should.be.eql('/');
+		sourcefile.symbols.should.not.be.empty;
+		sourcefile.symbols.should.have.length(1);
+		sourcefile.symbols[0].should.have.property('type');
+		sourcefile.symbols[0].should.have.property('value');
+		sourcefile.symbols[0].type.should.be.eql('compiler');
+		sourcefile.symbols[0].value.should.be.eql({});
+		sourcefile.symbols[0].source.should.be.eql('hyperloop_compiler({})');
+	});
+
+	it("should transform complex @native", function() {
+		var obj = {cflags:{'-DDEBUG':1}},
+			source = "@compiler(" + JSON.stringify(obj) + ")",
+			sourcefile = new MockSourceFile(),
+			ast = compiler.compile(source, 'test', sourcefile),
+			code = compiler.compress(ast,{},'test');
+		sourcefile.name.should.be.eql('test');
+		sourcefile.filename.should.be.eql('/test');
+		sourcefile.dirname.should.be.eql('/');
+		sourcefile.symbols.should.not.be.empty;
+		sourcefile.symbols.should.have.length(1);
+		sourcefile.symbols[0].should.have.property('type');
+		sourcefile.symbols[0].should.have.property('value');
+		sourcefile.symbols[0].type.should.be.eql('compiler');
+		sourcefile.symbols[0].source.should.be.eql('hyperloop_compiler'+toJS(obj));
 	});
 
 });
