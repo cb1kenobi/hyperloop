1ae2247a2a5177085ac66805fe8401252b4f603e
matt-langston <matt_langston@mac.com>
2013-09-24 11:20:20 -0700

    Initial Commit

---
+++ examples/ios/native/life/life/MDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDLAppDelegate.h
+//  life
+//
+//  Created by Matt Langston on 9/24/13.
+//  Copyright (c) 2013 Appcelerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ examples/ios/native/life/life/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  life
+//
+//  Created by Matt Langston on 9/24/13.
+//  Copyright (c) 2013 Appcelerator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDLAppDelegate class]));
+    }
+}
