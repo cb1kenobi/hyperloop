e34f6e72a2037445e3a00d49ad6ca0d57bed3b8a
matt-langston <matt_langston@mac.com>
2013-10-07 21:23:57 -0700

    Update to Modern Objective-C 2.0.

--- lib/ios/templates/AppDelegate.h
+++ lib/ios/templates/AppDelegate.h
@@ -6,15 +6,9 @@
  * This generated code and related technologies are covered by patents
  * or patents pending by Appcelerator, Inc.
  */
-@import Foundation;
 @import UIKit;
-@import JavaScriptCore;
-
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
-{
-	JSContextRef context;
-}
 
 @property (strong, nonatomic) UIWindow *window;
 
