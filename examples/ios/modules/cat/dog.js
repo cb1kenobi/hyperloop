0048ae83231ca9284df7ca3f3b3ac63a88cfb880
Pier Paolo Ramon <ramonpierre@gmail.com>
2013-12-09 03:07:06 +0100

    Make 'tests' fail for #161

--- examples/ios/modules/cat/dog.js
+++ examples/ios/modules/cat/dog.js
@@ -1,6 +1,6 @@
-globals.assert(module.filename==='cat/dog.js',"module.filename should be 'cat/dog.js'",module.filename);
-globals.assert(module.parent.id==='app',"module.parent.id should be 'app'",module.parent.id);
-globals.assert(module.parent.loaded===false,"module.parent.loaded should be false",module.parent.loaded);
+global.assert(module.filename==='cat/dog.js',"module.filename should be 'cat/dog.js'",module.filename);
+global.assert(module.parent.id==='app',"module.parent.id should be 'app'",module.parent.id);
+global.assert(module.parent.loaded===false,"module.parent.loaded should be false",module.parent.loaded);
 
 
 
