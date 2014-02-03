9c436ee0ea1402692889c315d2e726db3a83eee4
Dawson Toth <dtoth@appcelerator.com>
2014-02-03 17:51:17 -0500

    Ref #219: Add Native Example of Async HTTP GET

---
+++ examples/native/windows/BackgroundTransfer/BackgroundTransfer/Main.cpp
@@ -0,0 +1,54 @@
+/*
+ * Performs an asynchronous GET request to openweathermap.org.
+ */
+#include <Windows.h>
+#include <ppltasks.h>
+
+using namespace concurrency;
+using namespace Platform;
+using namespace Windows::Web::Http;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::Foundation;
+using namespace Windows::ApplicationModel::Activation;
+
+ref class MyApp sealed : public ::Application
+{
+public:
+	MyApp();
+	virtual void OnLaunched(LaunchActivatedEventArgs^ args) override;
+private:
+	TextBlock ^tb;
+};
+
+MyApp::MyApp(){}
+
+void MyApp::OnLaunched(LaunchActivatedEventArgs^ args)
+{
+	Window^ window = Window::Current;
+
+	tb = ref new TextBlock();
+	tb->Text = "Downloading, please wait...";
+	tb->FontSize = 40;
+	tb->TextWrapping = TextWrapping::Wrap;
+	tb->VerticalAlignment = VerticalAlignment::Center;
+	tb->HorizontalAlignment = HorizontalAlignment::Center;
+	window->Content = tb;
+
+	window->Activate();
+
+	auto uri = ref new Uri("http://api.openweathermap.org/data/2.5/weather?lat=37.389587&lon=-122.05037");
+	auto client = ref new HttpClient();
+	task<String ^>(client->GetStringAsync(uri)).then([&](String ^result) {
+		tb->Text = result;
+	});
+}
+
+int main(Platform::Array<Platform::String^>^)
+{
+	Application::Start(ref new ApplicationInitializationCallback([](ApplicationInitializationCallbackParams^ params) {
+		MyApp^ app = ref new MyApp();
+	}));
+
+	return 0;
+}
