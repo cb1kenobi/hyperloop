b616d06042262acfc2523214c28faca847a3a170
Dawson Toth <dtoth@appcelerator.com>
2014-02-10 14:45:47 -0500

    Ref #166: Propagate Windows Native Exceptions

--- lib/windows/wrl/templates/JSBuffer.cpp
+++ lib/windows/wrl/templates/JSBuffer.cpp
@@ -323,7 +323,8 @@ JSValueRef putForJSBuffer (JSContextRef ctx, JSObjectRef function, JSObjectRef o
         return JSValueMakeUndefined(ctx);
     }
 
-	throw ref new Exception(0, "JSBuffer's putForJSBuffer has not been implemented on Windows yet.");
+	HyperloopRaiseNativeToJSException(ctx, exception, ref new Exception(-1, "JSBuffer's putForJSBuffer has not been implemented on Windows yet."), __FILE__, __FUNCTION__, __LINE__);
+	return JSValueMakeUndefined(ctx);
     /*void *src = &(srcBuffer->buffer[(int)srcIndex]);
     size_t newsize = (buffer->length - (int)destIndex);
     newsize = newsize + srcLength - newsize;
@@ -868,7 +869,8 @@ JSValueRef sliceForJSBuffer (JSContextRef ctx, JSObjectRef function, JSObjectRef
         return JSValueMakeUndefined(ctx);
     }
 
-	throw ref new Exception(0, "JSBuffer's sliceForJSBuffer has not been implemented on Windows yet.");
+	HyperloopRaiseNativeToJSException(ctx, exception, ref new Exception(-1, "JSBuffer's sliceForJSBuffer has not been implemented on Windows yet."), __FILE__, __FUNCTION__, __LINE__);
+	return JSValueMakeUndefined(ctx);
     /*void *memory = malloc(length);
     void *start = &(buffer->buffer[(int)index]);
     memcpy(memory,start,length);
@@ -1197,7 +1199,7 @@ void DestroyJSBuffer(JSBuffer *buffer)
  */
 void SetJSBufferValue(JSContextRef ctx, JSObjectRef objectRef, JSValueRef sourceRef)
 {
-	throw ref new Exception(0, "JSBuffer's SetJSBufferValue has not been implemented on Windows yet.");
+	throw ref new Exception(-1, "JSBuffer's SetJSBufferValue has not been implemented on Windows yet.");
     /*JSBuffer *buffer = (JSBuffer*)HyperloopGetPrivateObjectAsJSBuffer(objectRef);
     if (buffer!=nullptr)
     {
